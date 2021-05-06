4ed65b884a34d87d48c089942c2e2a70caf1c26b
Shania <lsleguizamon9@misena.edu.co>
2021-05-05 22:59:14 -0500

    Arreglos multidimensionales

--- routes/web.php
+++ routes/web.php
@@ -29,11 +29,45 @@ Route::get("variables", function(){
 
 Route::get("arreglos", function (){
     //arreglo: estructura de datos declare
+    //an indice
     $estudiantes = [ "AN" => "Ana",
                      "NA" => "Maria" ,
                      "JO" => "Jorge"  ];
     echo "<pre>";
     print_r($estudiantes);
     echo "</pre>";
+
+});
+
+
+
+Route::get('paises' , function(){
+
+    $paises = [
+        "Colombia" => [
+            "Capital" => "Bogota",
+            "Moneda" => "Peso",
+            "Poblacion" => "50.34"],
+
+        "Peru" => [
+            "Capital" => "Lima",
+            "Moneda" => "Sol",
+            "Poblacion" => "32.84"],
+
+        "Paraguay " => [
+            "Capital" => "Asuncion",
+            "Moneda" => "Guarani",
+            "Poblacion" => "7"
+        ]
+    ];
+
+    foreach($paises as $pais ){
+
+    }
+
+    echo "<pre>";
+    print_r($pais);
+    echo "</pre>";
+    echo "<hr />";
 
 });
